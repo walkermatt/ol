8ba1c0ab376ed49c1283dfe555c63297dd1d934d
Tim Schaub <tim.schaub@gmail.com>
2013-11-20 17:16:10 -0700

    Add method to restore original feature attributes

--- src/ol/feature.js
+++ src/ol/feature.js
@@ -183,6 +183,30 @@ ol.Feature.prototype.setGeometry = function(geometry) {
 
 
 /**
+ * Reset all feature attributes to their original values.  This only works
+ * when the original values are set via `setOriginal`.
+ */
+ol.Feature.prototype.restoreOriginal = function() {
+  var original = this;
+  while (original.original_) {
+    original = original.original_;
+  }
+  if (original !== this) {
+    var oldValues = this.getAttributes();
+    var newValues = original.getAttributes();
+    this.setValues(newValues);
+    for (var key in oldValues) {
+      if (!newValues.hasOwnProperty(key)) {
+        // TODO: ol.Object.prototype.delete
+        this.set(key, undefined);
+      }
+    }
+  }
+  this.original_ = null;
+};
+
+
+/**
  * Gets the renderIntent for this feature.
  * @return {string} Render intent.
  */
